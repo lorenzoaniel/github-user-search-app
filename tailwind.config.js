1b40e19145e4f08aabe6af7c0665ce718bb0a7a7
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-21 15:42:20 -0600

    Initial setup, partial completeion of tailwindcss themes
    still working on typography themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	darkMode: "class",
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/app/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			//LIGHTMODE
+			backgroundColor: (theme) => ({
+				//extends tailwinds original pallete
+				...theme("colors"),
+				primary: "#FEFEFE",
+				secondary: "#F6F8FF",
+				"btn-primary": "#0079FF",
+			}),
+			textColor: (theme) => ({
+				...theme("colors"),
+				primary: "#4B6A9B",
+				secondary: "#2B3442",
+				"link-primary": "#0079FF",
+				toggle: "#697C9A",
+				"btn-primary": "#FEFEFE",
+			}),
+			//DARKMODE
+			dark: {
+				backgroundColor: (theme) => ({
+					//extends tailwinds original pallete
+					...theme("colors"),
+					primary: "#1E2A47",
+					secondary: "#141D2F",
+					"btn-primary": "#0079FF",
+				}),
+				textColor: (theme) => ({
+					...theme("colors"),
+					primary: "#FFFFFF",
+				}),
+			},
+		},
+	},
+	plugins: [],
+};
