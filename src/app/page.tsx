99e8aa5b98646277aeb30c959e151deda6af4841
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-23 20:46:00 -0600

    tablet size style updates for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,16 @@ import SearchButton from "@/components/button/SearchButton";
 import SearchBar from "@/components/searchbar";
 import AvatarDisplay from "@/components/display/AvatarDisplay";
 import StatsDisplay from "@/components/display/StatsDisplay";
+import LinksDisplay from "@/components/display/LinksDisplay";
+import Links from "@/components/links";
 
 const Home: React.FC = () => {
+	const userData = null;
 	const img_src = {
 		searchbaricon: "./assets/images/icon-search.svg",
 	};
+
+	//TODO when user clicks search button call useUserStore and grab the value of searchbar and place value into hook
 
 	return (
 		<>
@@ -22,6 +27,7 @@ const Home: React.FC = () => {
         flex
         items-center
         justify-between
+        md:w-[57.3rem]
         "
 			>
 				<h1
@@ -49,11 +55,16 @@ const Home: React.FC = () => {
         mt-[3.6rem]
         dark:bg-dark-primary
         dark:shadow-dark-container
+        md:w-[57.3rem]
+        md:h-[6.9rem]
         "
 			>
 				<Image
 					className="searchbar-icon
           ml-[0.85rem]
+          md:ml-[1.7rem]
+          md:h-[2.4rem]
+          md:w-[2.4rem]
           "
 					width={20}
 					height={20}
@@ -61,6 +72,29 @@ const Home: React.FC = () => {
 					alt=""
 				/>
 				<SearchBar />
+				<span
+					className="divider
+          hidden
+          w-[3rem]
+          h-full
+          md:inline
+          "
+				>
+					{""}
+				</span>
+				<span
+					className={`error-user-not-found
+          hidden
+          w-fit
+          text-body
+          text-red-500
+          md:opacity-0
+          md:inline
+          ${userData ?? "md:opacity-100"}
+          `}
+				>
+					{"No results"}
+				</span>
 				<SearchButton />
 			</nav>
 			<main
@@ -79,6 +113,8 @@ const Home: React.FC = () => {
         gap-y-[2.5rem]
         dark:bg-dark-primary
         dark:shadow-dark-container
+        md:w-[57.3rem]
+
         "
 			>
 				<AvatarDisplay
@@ -91,13 +127,21 @@ const Home: React.FC = () => {
 					className="main-section-bio
           h-[7.5rem]
           w-full
+          text-h4
           text-primary
           dark:text-dark-primary
+          dark:opacity-75
           "
 				>
 					{"TEST BIO INFO REPLACE WITH API RESPONSE"}
 				</p>
 				<StatsDisplay repocount={8} followercount={3938} followingcount={9} />
+				<LinksDisplay>
+					<Links variant={"location"} linkData={"San Francisco"} />
+					<Links variant={"blog"} linkData={null} />
+					<Links variant={"twitter"} linkData={null} />
+					<Links variant={"company"} linkData={null} />
+				</LinksDisplay>
 			</main>
 		</>
 	);
