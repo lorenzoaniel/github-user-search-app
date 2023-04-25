4cec79a5432c9b6572ee369e88d7bd3f4382f5d8
lorenzoaniel <lorenzoaniel@gmail.com>
2023-04-25 17:57:41 -0600

    cleanup

--- src/state_management/useUserStore.tsx
+++ src/state_management/useUserStore.tsx
@@ -8,14 +8,9 @@ type UserStore = {
 	fetchGithubUser: (username: string) => Promise<void>;
 	setUser: (user: UserType) => void;
 };
-/* 
-location?: string;
-  blog?: string;
-  twitter_username?: string;
-  company?: string | null; */
 
-const testbio =
-	"TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST ";
+// const testbio =
+// 	"TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST TEST TESTTESTTEST ";
 
 export const useUserStore = create<UserStore>((set, get) => ({
 	user: {
@@ -23,7 +18,7 @@ export const useUserStore = create<UserStore>((set, get) => ({
 		name: "The Octocat",
 		created_at: "2011-01-25T18:44:36Z",
 		avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
-		bio: testbio,
+		bio: "This profile has no bio",
 		public_repos: 8,
 		followers: 3938,
 		following: 9,
